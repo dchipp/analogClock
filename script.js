07bbff640d776b851245331b8755559f53b2fb4f
dchipp <itsjustpk.757@gmail.com>
2022-12-15 11:49:30 +0100

    Updated

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let hourLine = document.getElementById("hourLine");
 let minuteLine = document.getElementById("minuteLine");
-let date = new Date();
-let angle = 0;
-let a;
-let b;
-let c;
+let secondLine = document.getElementById("secondLine");
+let day = document.getElementById("day");
+let date = document.getElementById("date");
+let currentDate = new Date();
+let days = ['Domenica','Lunedì', 'Martedì','Mercoledì','Giovedì','Venerdì','Sabato']
 setInterval(()=>{
-    date=new Date();
-    console.log(date.getSeconds())
-    hourLine.style.transform = `translateX(50%) rotate(${((360*(date.getHours()%12))/12)-90+7+(((((360*(date.getMinutes()))/60)-90)*30)/360)}deg)`;
-    minuteLine.style.transform = `translateX(50%) rotate(${((360*(date.getMinutes()))/60)-90}deg)`; 
+    currentDate=new Date();
+    hourLine.style.transform = `rotate(${((360*(currentDate.getHours()%12))/12)-90+(currentDate.getMinutes()*0.5)}deg)`;
+    minuteLine.style.transform = `rotate(${((360*(currentDate.getMinutes()))/60)-90}deg)`; 
+    secondLine.style.transform = `rotate(${((360*(currentDate.getSeconds()))/60)-90}deg)`;
+    day.innerText=days[currentDate.getDay()];
+    date.innerText=`${currentDate.getDate()}/${currentDate.getMonth()+1}/${currentDate.getFullYear()}`;
 },10)
